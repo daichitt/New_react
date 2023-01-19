560e8bbd5a057d72c7e56adfaf7f19121ad4e16e
daichitt <gene.sixteen.000925@outlook.jp>
2023-01-19 15:10:43 +0900

    delete files no need

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
+    <div className="App">hello react 
     </div>
   );
 }
